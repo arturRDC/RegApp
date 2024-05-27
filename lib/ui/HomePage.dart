70b925cd90fcb7f431887647caaba068a6ec2740
Artur Ribeiro da Cunha <artur.rdc1@gmail.com>
2024-05-27 14:50:56 -0300

    Add some home page styles

--- lib/ui/HomePage.dart
+++ lib/ui/HomePage.dart
@@ -12,16 +12,35 @@ class _HomePageState extends State<HomePage> {
   int currentPageIndex = 0;
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Center(
-          child: Text(
-            'Home',
-            style: Theme.of(context).textTheme.displayMedium,
+    return Padding(
+      padding: const EdgeInsets.all(24.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Bom dia, ' 'User',
+            style: Theme.of(context).textTheme.headlineLarge,
           ),
-        ),
-      ],
+          Padding(
+            padding: const EdgeInsets.only(top: 32),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('Próximas irrigações',
+                    style: Theme.of(context).textTheme.headlineSmall),
+                const Icon(Icons.arrow_forward_ios)
+              ],
+            ),
+          ),
+          // Add the Listview here
+          Padding(
+            padding: const EdgeInsets.only(top: 32),
+            child: Text('Clima agora',
+                style: Theme.of(context).textTheme.headlineSmall),
+          )
+        ],
+      ),
     );
   }
 }
