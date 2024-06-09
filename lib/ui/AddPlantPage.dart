1f4e52fef44b2a338db671f5e7818f8dbcde937c
Artur Ribeiro da Cunha <artur.rdc1@gmail.com>
2024-06-09 12:22:54 -0300

    Allow adding image to AddPlantPage

--- lib/ui/AddPlantPage.dart
+++ lib/ui/AddPlantPage.dart
@@ -1,20 +1,105 @@
 import 'package:flutter/material.dart';
-import 'package:regapp/ui/components/IrrigationList.dart';
+import 'package:file_picker/file_picker.dart';
 
-class AddPlantPage extends StatelessWidget {
+class AddPlantPage extends StatefulWidget {
   const AddPlantPage({super.key});
+
+  @override
+  _AddPlantPageState createState() => _AddPlantPageState();
+}
+
+class _AddPlantPageState extends State<AddPlantPage> {
+  String fileName = '';
+  PlatformFile? pickedFile;
+
+  void openFilePicker() async {
+    FilePickerResult? result =
+        await FilePicker.platform.pickFiles(withData: true);
+
+    if (result != null) {
+      setState(() {
+        pickedFile = result.files.first;
+        fileName = pickedFile!.name;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            "Adicionar Planta",
-            style: Theme.of(context).textTheme.headlineMedium,
+      appBar: AppBar(
+        title: Text(
+          "Adicionar Planta",
+          style: Theme.of(context).textTheme.headlineMedium,
+        ),
+      ),
+      body: Form(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: SizedBox(
+                  height: 180,
+                  width: 180,
+                  child: Stack(
+                    children: [
+                      ClipOval(
+                        child: Container(
+                          color: Colors.blue,
+                        ),
+                      ),
+                      AspectRatio(
+                        aspectRatio: 1.0,
+                        child: ClipOval(
+                          child:
+                              (pickedFile != null && pickedFile!.bytes != null)
+                                  ? Image.memory(
+                                      pickedFile!.bytes!,
+                                      fit: BoxFit.cover,
+                                    )
+                                  : Padding(
+                                      padding: const EdgeInsets.all(12.0),
+                                      child: Image.asset(
+                                        'assets/icons/pottedPlantIcon.png',
+                                      ),
+                                    ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              TextFormField(
+                readOnly: true,
+                onTap: openFilePicker,
+                decoration: InputDecoration(
+                  border: const OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(4))),
+                  labelText: 'Escolha uma imagem',
+                  suffixIcon: const Icon(Icons.attach_file),
+                  hintText:
+                      fileName.isEmpty ? 'Imagem não selecionada' : fileName,
+                ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 0, vertical: 16),
+                child: SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // Handle form submission
+                    },
+                    child: const Text('Salvar'),
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
-        body: const Center(
-          child: Text("TODO: Adicionar Planta"),
-        ));
+      ),
+    );
   }
 }
