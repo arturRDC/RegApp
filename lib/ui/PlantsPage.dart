28439afe63b29383970635142442bc184628b50f
Artur Ribeiro da Cunha <artur.rdc1@gmail.com>
2024-06-09 14:27:52 -0300

    Fix scrolling and gradient on plants page

--- lib/ui/PlantsPage.dart
+++ lib/ui/PlantsPage.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:regapp/ui/components/IrrigationList.dart';
 import 'package:regapp/ui/components/PlantList.dart';
-import 'package:regapp/ui/components/WeatherCard.dart';
-import 'package:regapp/ui/components/WeatherList.dart';
 
 class PlantsPage extends StatefulWidget {
   const PlantsPage({super.key});
@@ -16,44 +13,40 @@ class _PlantsPageState extends State<PlantsPage> {
   int currentPageIndex = 0;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SizedBox(
-        height: double.infinity,
-        child: Column(
-          children: [
-            Expanded(
-              child: SingleChildScrollView(
-                child: Padding(
-                  padding: const EdgeInsets.all(24.0),
-                  child: PlantList(),
+    return SizedBox(
+      height: double.infinity,
+      child: Column(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(24.0),
+              child: PlantList(),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(24, 24, 24, 48),
+            child: SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () => context.push('/plants/addPlant'),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.fromLTRB(0, 0, 8, 0),
+                      child: Icon(Icons.add),
+                    ),
+                    Text(
+                      'Adicionar planta',
+                      style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                          color: Theme.of(context).colorScheme.onPrimary),
+                    ),
+                  ],
                 ),
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(24, 24, 24, 48),
-              child: SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: () => context.push('/plants/addPlant'),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Padding(
-                        padding: EdgeInsets.fromLTRB(0, 0, 8, 0),
-                        child: Icon(Icons.add),
-                      ),
-                      Text(
-                        'Adicionar planta',
-                        style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-                            color: Theme.of(context).colorScheme.onPrimary),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
