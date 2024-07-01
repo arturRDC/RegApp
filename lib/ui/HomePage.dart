306c1ba9a0b44f52fdd468311f505b4dc2d159d2
Artur Ribeiro da Cunha <artur.rdc1@gmail.com>
2024-07-01 15:31:09 -0300

    Add current weather to main page

--- lib/ui/HomePage.dart
+++ lib/ui/HomePage.dart
@@ -1,5 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:geocoding/geocoding.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:go_router/go_router.dart';
 import 'package:regapp/ui/components/IrrigationList.dart';
 import 'package:regapp/ui/components/WeatherCard.dart';
@@ -13,6 +17,74 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   int currentPageIndex = 0;
+
+  Map<String, String> weatherNow = {
+    'title': '',
+    'location': '',
+    'weekDay': '',
+    'rainPct': ''
+  };
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchWeatherData();
+  }
+
+  Future<String> getLocationName(Position position) async {
+    List<Placemark> placemarks = await placemarkFromCoordinates(
+      position.latitude,
+      position.longitude,
+    );
+
+    Placemark placemark = placemarks[0];
+
+    String locationName =
+        '${placemark.subAdministrativeArea!} - ${placemark.isoCountryCode!}';
+    return locationName;
+  }
+
+  Future<void> fetchWeatherData() async {
+    try {
+      Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.low);
+      String locationName = await getLocationName(position);
+
+      final response = await http.get(Uri.parse(
+          'https://api.open-meteo.com/v1/forecast?latitude=${position.latitude}&longitude=${position.longitude}&current_weather=true&hourly=temperature_2m,relativehumidity_2m,precipitation_probability'));
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        setState(() {
+          weatherNow = {
+            'title': getWeatherTitle(data['current_weather']['weathercode']),
+            'location': locationName,
+            'weekDay': DateTime.now().weekday.toString(),
+            'rainPct':
+                data['hourly']['precipitation_probability'][0].toString(),
+          };
+          isLoading = false;
+        });
+      } else {
+        throw Exception('Failed to load weather data');
+      }
+    } catch (e) {
+      print('Error fetching weather data: $e');
+      setState(() {
+        isLoading = false;
+      });
+    }
+  }
+
+  String getWeatherTitle(int weatherCode) {
+    if (weatherCode == 0 || weatherCode == 1) {
+      return 'Ensolarado';
+    } else if (weatherCode < 50) {
+      return 'Nublado';
+    } else {
+      return 'Chuvoso';
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -67,13 +139,12 @@ class _HomePageState extends State<HomePage> {
               child: Text('Clima agora',
                   style: Theme.of(context).textTheme.headlineSmall),
             ),
-            const Padding(
-              padding: EdgeInsets.symmetric(vertical: 8.0),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
               child: WeatherCard(
-                title: 'Ensolarado',
-                rainPct: '15',
-                location: 'Natal, RN',
-              ),
+                  title: weatherNow['title']!,
+                  rainPct: weatherNow['rainPct']!,
+                  location: weatherNow['location']!),
             ),
           ],
         ),
