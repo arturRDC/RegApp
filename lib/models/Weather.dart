9fb7bd6e675bb727b908815953d95c24be8d3e60
Ilo Benevides <86765486+Astospam@users.noreply.github.com>
2024-06-30 23:27:40 -0300

    Weather Base
    Criação da classe modelo weather, modificação de permissão e dependencias e modificação da WeatherPage

---
+++ lib/models/Weather.dart
@@ -0,0 +1,11 @@
+class Weather{
+  final String weather;
+  final String temperature;
+  final String rainPct;
+
+  Weather({
+    required this.weather,
+    required this.temperature,
+    required this.rainPct,
+  });
+}
--- lib/ui/WeatherPage.dart
+++ lib/ui/WeatherPage.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:regapp/ui/components/WeatherCard.dart';
 import 'package:regapp/ui/components/WeatherList.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:regapp/models/Weather.dart';
+import 'package:geolocator/geolocator.dart';
 
 class WeatherPage extends StatefulWidget {
   const WeatherPage({super.key});
@@ -10,6 +14,41 @@ class WeatherPage extends StatefulWidget {
 }
 
 class _WeatherPageState extends State<WeatherPage> {
+
+  Weather weather = Weather(weather: "", temperature: "", rainPct: "");
+
+  late String lat;
+  late String long;
+
+  Future <Position> _getCurrentLocation() async {
+    bool serviceEnaled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnaled) {
+      return Future.error('Serviços de localização estão desabiilitados.');
+    }
+
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Permissões de localização estão permanentemente negadas, incapaz de solicitar.');
+      }
+    }
+
+    return await Geolocator.getCurrentPosition();
+  }
+
+  Future getWeather() async{
+    _getCurrentLocation().then((value) {
+      lat = '${value.latitude}';
+      long = '${value.longitude}';
+    });
+    var response = await http.get(Uri.https('api.open-meteo.com', 'v1/forecast?latitude=' + lat + '&longitude=' + long + '&current=temperature_2m,weather_code&daily=precipitation_probability_max&forecast_days=1'));
+    var jsonData = jsonDecode(response.body);
+    var currentWeather = jsonData['current'];
+    var dailyWeather = jsonData['daily'];
+    weather = Weather(weather: currentWeather['weather_code'], temperature: currentWeather['temperature_2m'], rainPct: dailyWeather['precipitation_probability_max']);
+  }
+  
   int currentPageIndex = 0;
   final Map<String, String> weatherNow = {
     'title': 'Ensolarado',
@@ -22,19 +61,31 @@ class _WeatherPageState extends State<WeatherPage> {
     return SingleChildScrollView(
       child: Padding(
         padding: const EdgeInsets.all(24.0),
-        child: Column(
+        child: FutureBuilder(
+          future: getWeather(),
+          builder:(context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text('Hoje', style: Theme.of(context).textTheme.headlineSmall),
             WeatherCard(
                 title: weatherNow['title']!,
-                rainPct: weatherNow['rainPct']!,
+                rainPct: weatherNow[weather.rainPct]!,
                 location: weatherNow['location']!),
             Text('Esta semana',
                 style: Theme.of(context).textTheme.headlineSmall),
             WeatherList(),
           ],
-        ),
+        );
+            }
+            else {
+              return Center(child: CircularProgressIndicator(),);
+            }
+            throw Exception();
+          },
+          
+        )
       ),
     );
   }
